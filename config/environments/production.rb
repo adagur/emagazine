83d32408458e462afd53bf4264a45c7f3f5b2a39
adagur <adagur@sapient.com>
2015-08-06 07:24:50 +0000

    html and assets changes for welcome and login popup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  ENV['USERNAME'] = 'eshop_prod'
+  ENV['PASSWORD'] = 'password'
   # Code is not reloaded between requests.
   config.cache_classes = true
 
